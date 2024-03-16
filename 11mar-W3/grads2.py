5a04fa7cc9cd4c457910a6776adae9093f98e85c
nabilbachroin <abiel769@gmail.com>
2024-03-16 16:23:11 +0800

    first commit

---
+++ 11mar-W3/grads2.py
@@ -0,0 +1,40 @@
+import matplotlib.animation as animation
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gradient_descent2(gradient, start, learn_rate, n_iter=50): 
+    tolerance=1e-03
+    vector = start
+    yield vector
+    for _ in range(n_iter):
+        step = -learn_rate * gradient(vector)
+        if np.all(np.abs(step) <= tolerance): #check that the difference is small enough
+            break
+        vector += step
+        yield vector
+    #return results
+
+def C(x):
+    return (x-2)*(x-2)
+
+def gradC(v):
+    return 2.0*(v-2)
+
+def animate(i,gen):
+	vec=next(gen)
+	print(vec)
+	plt.plot(vec[0],C(vec[0]),'ro')
+
+def main():	
+	x=np.arange(-10,15)
+	y=C(x)
+	plt.plot(x,y)
+	start=np.array([10.])
+	gen=gradient_descent2(gradC,start,0.2)
+	ani = animation.FuncAnimation(plt.gcf(), animate,fargs=(gen,), interval=1000) 
+
+	plt.show()
+
+
+if __name__ == "__main__":
+    main()
